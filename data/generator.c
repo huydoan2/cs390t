8e38c6846eeda95a8ed2158a304037a2571fba59
Hangchen Yu <hyu@cs.utexas.edu>
2017-04-18 19:07:38 -0500

    Init input generator

---
+++ data/generator.c
@@ -0,0 +1,7 @@
+int main() {
+    // TODO: generate increasing sequence
+    // TODO: generate decreasing sequence
+    // TODO: generate interleaving sequence
+    // TODO: generate random sequence
+    return 0;
+}
